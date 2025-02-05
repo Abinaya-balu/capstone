c78d1a9834e33eb2859ac697f8ecd2f7940fcbea
Abinaya-balu <112405298+Abinaya-balu@users.noreply.github.com>
2025-02-05 11:31:14 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
     echo hi123
     sh 'chmod +x build.sh'
     sh './build.sh'
-    docker login -u abinayabalusamy -p dckr_pat_Bq4IXSo0vD479PzZ1G9ZgF40hwQ
+    docker login -u abinayabalusamy -p dckr_pat_JHYzC2wC8xUc2iq4-QSMIwHHkNs
     docker tag test abinayabalusamy/task2
     docker push abinayabalusamy/task2
     
